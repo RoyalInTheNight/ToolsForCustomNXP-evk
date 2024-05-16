83ac639cbbd69b28492fd85a823882e739ffb64d
Michael <107641578+RoyalInTheNight@users.noreply.github.com>
2024-05-16 12:44:28 +0000

    Update boot.sh

--- tools/boot.sh
+++ tools/boot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+caam-keygen import /data/caam/randomkey.bb importKey
 cat /data/caam/importKey | keyctl padd logon logkey: @s
 losetup /dev/loop0 /dev/mmcblk2p4
 dmsetup -v create encrypted --table "0 $(blockdev --getsz /dev/loop0) crypt capi:tk(cbc(aes))-plain :36:logon:logkey: 0 /dev/loop0 0 1 sector_size:512"
 mount /dev/mapper/encrypted /mnt/encrypted/
+
 
 /usr/local/tools/boot64_yt8512.sh
 /usr/local/tools/boot64_sim7600H.sh
